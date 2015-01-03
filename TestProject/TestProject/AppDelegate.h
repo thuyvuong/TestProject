eee22ae130dc83457f53da83ddb1f84b207954ef
TaiVuong <vhuutai@gmail.com>
2015-01-03 16:19:21 +0700

    Init project

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by TaiVuong on 1/3/15.
+//  Copyright (c) 2015 2359 Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by TaiVuong on 1/3/15.
+//  Copyright (c) 2015 2359 Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
